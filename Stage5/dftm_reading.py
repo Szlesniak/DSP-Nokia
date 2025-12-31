131f55de548ead1be1b745b26f6f8f3ee407b9bb
Szlesniak <szymles44@gmail.com>
2025-12-31 16:39:10 +0100

    final stage

--- Stage5/dftm_reading.py
+++ Stage5/dftm_reading.py
@@ -1,8 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
+from mylib import *
 
-N = 1000000
-rzuty = np.random.randint(1, 7, size=(N, 4))
+# loads samples from .wav file with exemplary DTFM signal
+# adapt file path
+samples = read(r'wav\d.wav') #b, c, d
+samplig_freq = samples[0]
+samples = samples[1]
+samples = samples[:1024]
+# plt.plot(samples)
+# plt.show()
+freqs = np.linspace(0,samplig_freq,len(samples))
 
-wyniki = np.sum(np.sort(rzuty, axis=1)[:, 1:], axis=1)
+real, imag = myDFT(samples)
+real_amplitudes = np.abs(real)
+imag_amplitudes = np.abs(imag)
 
-print(f"Średni wynik (NumPy): {np.mean(wyniki):.5f}")
+amplitudes = np.sqrt(real_amplitudes**2 + imag_amplitudes**2)
+plt.plot(freqs, amplitudes,label='d.wav')
+plt.grid()
+plt.legend()
+
+
+#culd be usefool for zooming
+plt.xlim(600,1700)
+plt.ylim(-30_000,3_0000)
+
+plt.show()
+
+
+
+
